d222c4d90d77ed18635b813e47febd61fd7007ed
Raj Kishore <rajkish321@gmail.com>
2019-05-24 18:38:22 -0400

    finished simple xylophone

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private int mGSoundId;
     private int mASoundId;
     private int mBSoundId;
+    private SoundPool mSoundPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,15 +32,59 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // TODO: Create a new SoundPool
-
+        mSoundPool = new SoundPool(NR_OF_SIMULTANEOUS_SOUNDS, AudioManager.STREAM_MUSIC, 0);
 
         // TODO: Load and get the IDs to identify the sounds
-
+        mCSoundId = mSoundPool.load(getApplicationContext(),R.raw.note1_c,PRIORITY);
+        mDSoundId = mSoundPool.load(getApplicationContext(),R.raw.note2_d,PRIORITY);
+        mESoundId = mSoundPool.load(getApplicationContext(),R.raw.note3_e,PRIORITY);
+        mFSoundId = mSoundPool.load(getApplicationContext(),R.raw.note4_f,PRIORITY);
+        mGSoundId = mSoundPool.load(getApplicationContext(),R.raw.note5_g,PRIORITY);
+        mASoundId = mSoundPool.load(getApplicationContext(),R.raw.note6_a,PRIORITY);
+        mBSoundId = mSoundPool.load(getApplicationContext(),R.raw.note7_b,PRIORITY);
 
     }
 
-    // TODO: Add the play methods triggered by the buttons
 
+    public void playC(View v) {
+        mSoundPool.play(mCSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+
+    }
+
+    public void playD(View v) {
+        mSoundPool.play(mDSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+
+    }
+
+    public void playE(View v) {
+        mSoundPool.play(mESoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+
+    }
+
+    public void playF(View v) {
+        mSoundPool.play(mFSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+
+    }
+
+    public void playG(View v) {
+        mSoundPool.play(mGSoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+
+    }
+
+    public void playA(View v) {
+        mSoundPool.play(mASoundId, LEFT_VOLUME, RIGHT_VOLUME, PRIORITY, NO_LOOP, NORMAL_PLAY_RATE);
+
+    }
+    public void playB(View v){
+        mSoundPool.play(mBSoundId,LEFT_VOLUME,RIGHT_VOLUME,PRIORITY,NO_LOOP,NORMAL_PLAY_RATE);
+
+    }
 
 
 }
+
+// TODO: Add the play methods triggered by the buttons
+
+
+
+
